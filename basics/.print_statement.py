2563d30603d1e3c860ad03703de55d4ccffd4015
Mukesh Kumar Mahto <mukesh@gmail.com>
2026-09-12 00:19:25 +0530

    code is submitted

---
+++ basics/.print_statement.py
@@ -0,0 +1,14 @@
+'''In this task, you are required to create a program that displays the given info:
+
+Name: "Alice"
+Favorite Color: "Blue"
+Favorite Hobby: "Reading"
+Your program should output the following format:
+
+My name is Alice.
+My favorite color is Blue.
+My favorite hobby is Reading.'''
+
+
+print("My name is Alice.","My favorite color is Blue.","My favorite hobby is Reading.",sep='\n');
+
