b14993493e395f98f48d6535dcd585af34208186
Mukesh Kumar Mahto <mukesh@gmail.com>
2026-09-12 20:24:43 +0530

    submit

---
+++ basics/day 2/print_end_parameter.py
@@ -0,0 +1,9 @@
+#Print each part of a folder path on the same line, using / after each part.
+
+# Expected Output C:/Users/John/Documents
+
+
+print("C:",end="/")
+print("Users",end="/")
+print("John",end="/")
+print("Documents")
