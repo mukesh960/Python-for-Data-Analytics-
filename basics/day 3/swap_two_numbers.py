ed63df96a315bf961fcc9f91f7a36b97a6224c6c
Mukesh Kumar Mahto <iamlazyboy123@gmail.com>
2026-09-13 17:32:30 +0530

    Update swap two numbers program

--- basics/day 3/swap_two_numbers.py
+++ basics/day 3/swap_two_numbers.py
@@ -1,13 +1,31 @@
 # @title Default title text
 # Write your code here
-a=10
-b=20
+a=int(input("Enter a first number: "))
+b=int(input("Enter a second number: "))
 
 
 print("Before swapping")
 print("a: ",a)
 print("b: ",b)
-#swappping each other
+a,b=b,a
+print("After wapping")
+print("a: ",a)
+print("b: ",b)
+
+
+
+
+
+
+# @title Default title text
+# Write your code here
+a=int(input("Enter a first number: "))
+b=int(input("Enter a second number: "))
+
+
+print("Before swapping")
+print("a: ",a)
+print("b: ",b)
 a,b=b,a
 print("After wapping")
 print("a: ",a)
