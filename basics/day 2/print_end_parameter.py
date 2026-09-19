f31f6991796f40a0a5d85fa703cecd0efece8470
Mukesh Kumar Mahto <iamlazyboy123@gmail.com>
2026-09-20 00:01:07 +0530

    Add Python practice files

--- basics/day 2/print_end_parameter.py
+++
@@ -1,9 +0,0 @@
-#Print each part of a folder path on the same line, using / after each part.
-
-# Expected Output C:/Users/John/Documents
-
-
-print("C:",end="/")
-print("Users",end="/")
-print("John",end="/")
-print("Documents")
--- basics/day 2/print_separator.py
+++
@@ -1,9 +0,0 @@
-'''In this task, you are required to fix errors in the provided code that uses the print() function incorrectly. The errors are due to the improper placement of the sep parameter, causing the output to differ from what’s expected.
-
-Your program should ultimately output the following format:
-
-Python, is, awesome
-Let's, learn, to, code.'''
-
-print("Python","is","awesome",sep=', ')
-print("Let's","learn","to","code.",sep=', ')
